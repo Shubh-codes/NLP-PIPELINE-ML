009a95e100d62387c7859ae656051380cff3a9e1
Shubh A <60384581+Shubh-codes@users.noreply.github.com>
2024-02-08 00:33:59 +0530

    Fixed some bugs and added the about page. api page

--- app.py
+++ app.py
@@ -1,87 +1,60 @@
-
-from string import punctuation
-from flask import Flask,url_for,request,render_template,jsonify,send_file
+from flask import Flask, url_for, request, render_template, jsonify, send_from_directory
 from flask_bootstrap import Bootstrap
-import json
-import nltk
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('maxent_ne_chunker')
-nltk.download('averaged_perceptron_tagger')
-import re
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.stem.porter import PorterStemmer
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk import pos_tag, ne_chunk
-from nltk.tokenize import word_tokenize
-import string
-from collections import Counter
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import TweetTokenizer
 import spacy
-nltk.download('words')
-from spacy import tokens
-from textblob import TextBlob 
+import time
+from textblob import TextBlob
+
+# Initialize the Spacy NLP model
 nlp = spacy.load("en_core_web_sm")
 
-# WordCloud & Matplotlib Packages
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt 
-from io import BytesIO
-import random
-import time
-
-
-# Initialize App
+# Initialize Flask app
 app = Flask(__name__)
 Bootstrap(app)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-
-@app.route('/analyze',methods=['GET','POST'])
-# We will lower case the enter data in the rawtext
+@app.route('/analyze', methods=['GET', 'POST'])
 def analyze():
-	start = time.time()
-	# Receives the input query from form
-	if request.method == 'POST':
-		rawtext = request.form['rawtext']
-		# Analysis
-		docx = nlp(rawtext)
-		# Tokens
-		custom_tokens = [token.text for token in docx ]
-		# Word Info
-		custom_wordinfo = [(token.text,token.lemma_,token.is_stop) for token in docx ]
-		lowered_text = rawtext.lower()
-		length_text = str(len(rawtext))
-		remove_punctuation = [token for token in docx if not token.is_punct]
-		' '.join(token.text for token in remove_punctuation)
-		
-		remove_whitespace = rawtext.replace("   " , "").strip()
-		
+    if request.method == 'POST':
+        start = time.time()
+        
+        # Processing the text
+        rawtext = request.form['rawtext']
+        docx = nlp(rawtext)
+        custom_tokens = [token.text for token in docx]
+        custom_wordinfo = [(token.text, token.lemma_, token.is_stop) for token in docx]
+        lowered_text = rawtext.lower()
+        length_text = str(len(rawtext))
+        remove_punctuation = [token.text for token in docx if not token.is_punct]
+        
+        custom_postagging = [(word.text, word.tag_, word.pos_, word.dep_) for word in docx]
+        custom_namedentities = [(entity.text, entity.label_) for entity in docx.ents]
+        
+        blob = TextBlob(rawtext)
+        blob_sentiment, blob_subjectivity = blob.sentiment.polarity, blob.sentiment.subjectivity
+        
+        end = time.time()
+        final_time = end - start
+        
+        return render_template('index.html', ctext=rawtext, custom_tokens=custom_tokens, length_text=length_text, remove_punctuation=remove_punctuation, custom_postagging=custom_postagging, custom_namedentities=custom_namedentities, custom_wordinfo=custom_wordinfo, lowered_text=lowered_text, blob_sentiment=blob_sentiment, blob_subjectivity=blob_subjectivity, final_time=final_time)
+    else:
+        return render_template('index.html')
 
+@app.route('/basic_api')
+def basic_api():
+    return render_template('restfulapidocs.html')
 
-		custom_postagging = [(word.text,word.tag_,word.pos_,word.dep_) for word in docx]
-		# NER
-		custom_namedentities = [(entity.text,entity.label_)for entity in docx.ents]
-		blob = TextBlob(rawtext)
-		blob_sentiment,blob_subjectivity = blob.sentiment.polarity ,blob.sentiment.subjectivity
-		# allData = ['Token:{},Tag:{},POS:{},Dependency:{},Lemma:{},Shape:{},Alpha:{},IsStopword:{}'.format(token.text,token.tag_,token.pos_,token.dep_,token.lemma_,token.shape_,token.is_alpha,token.is_stop) for token in docx ]
-		allData = [('"Token":"{}","Tag":"{}","POS":"{}","Dependency":"{}","IsStopword":"{}"'.format(token.text,token.tag_,token.pos_,token.dep_,token.is_stop)) for token in docx ]
+@app.route('/imagescloud')
+def imagescloud():
+    # Serve the images cloud page
+    return render_template('images.html')
 
-		result_json = json.dumps(allData, sort_keys = False, indent = 2)
-
-		end = time.time()
-		final_time = end-start
-	return render_template('index.html',ctext=rawtext,custom_tokens=custom_tokens,remove_whitespace = remove_whitespace ,length_text = length_text,remove_punctuation = remove_punctuation, custom_postagging=custom_postagging, custom_namedentities=custom_namedentities,custom_wordinfo=custom_wordinfo,lowered_text = lowered_text , blob_sentiment=blob_sentiment,blob_subjectivity=blob_subjectivity,final_time=final_time,result_json=result_json)
-
-
-
+@app.route('/about')
+def about():
+    # Serve the about page
+    return render_template('about.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
